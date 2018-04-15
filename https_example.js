9bb98852674803135b58c588e42fad306b7dab8c
Ryan Olejnik <ryan.olejnik@gmail.com>
2018-04-15 20:40:33 +0000

    added https request

--- https_example.js
+++ https_example.js
@@ -1,4 +1,18 @@
 var https = require('https');
 
 console.log('i did it!');
+var options = {
+  host: 'www.google.ca',
+  path: '/'
+};
 
+// called by https when the request is made.
+var callback = function() {
+  console.log('In response handler callback!');
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
